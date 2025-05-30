f198deda6759cb267d36be55fac877148df2995a
Rumwisai Tinotenda Chivasa <rumwisaichivasa@gmail.com>
2025-05-30 08:37:35 +0200

    Friday

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ const local = 'http://localhost:5000'
 const production = 'https://nimbo-backend-1.onrender.com'
 
 let api_url =''
-let mode ='pro''
+let mode ='pro'
 
 if (mode === 'pro'){
     api_url =production
